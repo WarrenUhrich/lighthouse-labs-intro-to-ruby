a477b96eab8ca6cce53226d329f4b7a8191c4d71
Warren <hello@warren.codes>
2022-04-29 09:20:54 -0600

    Add comments example; add printing to terminal example.

---
+++ 01-comments.rb
@@ -0,0 +1,9 @@
+# This is a single-line Ruby comment.
+# It is technically the only character intended for commenting.
+
+=begin
+This is an
+embedded document.
+
+It can be used for multi-line comments though!
+=end
---
+++ 02-outputting-to-terminal.rb
@@ -0,0 +1,13 @@
+# Print is a simple way to output to terminal.
+print "This is a string! " # Print doesn't force a new-line, so it should be used carefully.
+
+# Puts adds a new-line, so it is usually easier to read output.
+puts "This one ends with a new-line!"
+
+# Inspect shows us the raw value, good for differentiating between strings vs. numbers, etc.
+puts "Hello, World!".inspect
+
+# Short form of "puts value.inspect" is "p"
+p "Hello, World!"
+
+p 3
