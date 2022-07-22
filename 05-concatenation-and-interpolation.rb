3031cd39ebfca99caae89425e7e3382436e85482
Warren <hello@warren.codes>
2022-07-22 09:28:18 -0600

    Add concatenation example.

---
+++ 05-concatenation-and-interpolation.rb
@@ -0,0 +1,12 @@
+welcome_text = "Hello, and welcome "
+welcome_name = "Ann"
+
+welcome_full = welcome_text + welcome_name + '!'
+puts welcome_full
+
+puts 'My favourite number is: ' + 3.to_s
+# console.log('My favourite number is ' + 3);
+
+# `${}`
+
+puts "Howdy, how are ya #{welcome_name}? Great day to Program! My favourite number is still #{3}."
